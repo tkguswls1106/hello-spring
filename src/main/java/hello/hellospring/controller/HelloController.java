3034ff36aa17d1f8870bd8ea09a82dc246520688
sahyunjin <tkguswls1106@naver.com>
2022-08-03 00:00:43 +0900

    인프런 spring 강의 'MVC와 템플릿 엔진'까지 시청 완료함.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";  // (ViewName)+.html 이므로 hello.html 파일인 View 파일을 가리키게되고,
                          // 위의 data:hello!! 값을 templates_hello.html 파일로 렌더링함.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {  // 쿼리파라미터 /hello-mvc?name=shj 이런식으로 적어주면 된다. 참고로 쿼리파라미터는 get방식이다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
